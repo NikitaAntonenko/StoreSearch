3d38981c9d17c24c37f4a4e7c1532ffa2143efb7
Nikita <ukr_vezunchik@ukr.net>
2018-02-07 08:51:55 +0200

    Creating the UI

---
+++ StoreSearch/SearchViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by getTrickS2 on 2/7/18.
+//  Copyright © 2018 Nik's. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    // MARK: - Outlets ==================================
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    // ==================================================
+    
+    // MARK: - Override functions ==========================================
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tableView.contentInset = UIEdgeInsets(top: 76, left: 0, bottom: 0, right: 0)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    // ======================================================================
+
+}
+
